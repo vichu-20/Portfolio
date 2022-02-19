756f9d4db2fbc2b55caf7eadab557a4abecdce5a
vichu-20 <ivichu20@gmail.com>
2022-02-19 22:21:51 +0530

    mobile view icons added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import './App.css';
 import Moon from './assets/moon.png'
 import Background from './assets/bg-dark.png'
 import Photo from './assets/photo.png'
+import Gmail from './assets/gmail.png'
+import Telegram from './assets/telegram.png'
+import Twitter from './assets/twitter.png'
+import Instagram from './assets/instagram.png'
+import Behance from './assets/behance.png'
+import Linkedin from './assets/linkedin.png'
+import Html from './assets/html-5.png'
+import Css from './assets/css.png'
+import Js from './assets/js.png'
+import React from './assets/structure.png'
+import Linux from './assets/linux.png'
+import Git from './assets/git.png'
+import Sketch from './assets/sketch.png'
+import Figma from './assets/figma.png'
+import Xd from './assets/xd.png'
+import Illustrator from './assets/illustrator.png'
+
+
 
 function App() {
   return (
@@ -26,7 +44,29 @@ function App() {
           <img src={Photo}/>
         </div>
         <div className='Details'>
-          <div className='Social'></div>
+          <div className='Social'>
+            <img src={Gmail}/>
+            <img src={Telegram}/>
+            <img src={Twitter}/>
+            <img src={Instagram}/>
+            <img src={Behance}/>
+            <img src={Linkedin}/>
+          </div>
+          <div className='Knowledge'>
+            <span className='Knowledge-title'>Knowledge in:</span>
+            <div className='Icons'>
+            <img src={Html}/>
+            <img src={Css}/>
+            <img src={Js}/>
+            <img src={React}/>
+            <img src={Linux}/>
+            <img src={Git}/>
+            <img src={Sketch}/>
+            <img src={Figma}/>
+            <img src={Xd}/>
+            <img src={Illustrator}/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
