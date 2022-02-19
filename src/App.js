84947f1da5b418a62e6314a5da157a1afaa16173
vichu-20 <ivichu20@gmail.com>
2022-02-19 17:09:37 +0530

    mobile view half completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Moon from './assets/moon.png'
+import Background from './assets/bg-dark.png'
+import Photo from './assets/photo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Top'>
+        <span className='Portfolio'>Portfolio</span>
+        <img src={Moon} />
+      </div>
+      <div className='Header'>
+        <span className='Greeting'>Hey there...</span>
+        <div className='Name'>
+          <span className='Name-1'>it's</span>
+          <span className='Name-2'>Athul Vishnu</span>
+        </div>
+        <span className='Position'> Front End Developer & Designer</span>
+      </div>
+      <div className='Main'>
+        <div style={{backgroundImage:`url(${Background})`
+        ,backgroundRepeat:"no-repeat",backgroundSize:"contain",
+        marginLeft:20,marginTop:20 ,}}
+         className='Profile-pic'>
+          <img src={Photo}/>
+        </div>
+        <div className='Details'>
+          <div className='Social'></div>
+        </div>
+      </div>
     </div>
   );
 }
